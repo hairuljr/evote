2aef578e80a98919e75b926a924c7501d34b0b99
Hairul Jr <januarhairul@gmail.com>
2022-01-20 10:16:04 +0700

    Refactor, Style : Navbar Brand Image Size

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -26,7 +26,7 @@
         <nav class="navbar navbar-expand-lg navbar-light fixed-top shadow-sm" id="mainNav">
             <div class="container px-5">
                 <a class="navbar-brand fw-bold text-center" href="#page-top">
-                    <img class="w-50" src="{{ config('ladmin.logo') }}" alt="Logo Fesma">
+                    <img width="22%" src="{{ config('ladmin.logo') }}" alt="Logo Fesma">
                 </a>
             </div>
         </nav>
